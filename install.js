4b7a7e3bbd55880ba977f3512a63f7f2c18c71a2
Nassitch <Nassitch@hotmail.fr>
2024-09-18 17:43:37 +0200

    Fix the install.js script.

--- install.js
+++ install.js
@@ -4,9 +4,9 @@ const jsonPackagePath = "../../package.json";
 const packageJson = require(jsonPackagePath);
 const scriptPath = "sh node_modules/saturn-css/script.sh";
 
-packageJson.script.saturn = scriptPath;
+packageJson.scripts.saturn = scriptPath;
 
-if (packageJson.script.saturn) {
+if (packageJson.scripts.saturn) {
     fs.writeFileSync(jsonPackagePath, JSON.stringify(packageJson, null, 2));
 } else {
     console.error("The property is non-existent.");
