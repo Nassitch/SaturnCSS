7d65297661e94a2baedbd04561781e53ab49f15b
Nassitch <Nassitch@hotmail.fr>
2024-09-18 18:04:04 +0200

    Fix files install.js script and script.sh.

--- install.js
+++ install.js
@@ -3,10 +3,14 @@ const fs = require("fs");
 const jsonPackagePath = "../../package.json";
 const packageJson = require(jsonPackagePath);
 const scriptPath = "sh node_modules/saturn-css/script.sh";
+const saturn = "saturn-css";
 
-packageJson.scripts.saturn = scriptPath;
+delete packageJson.scripts.saturn;
+delete packageJson.scripts[saturn];
 
-if (packageJson.scripts.saturn) {
+packageJson.scripts[saturn] = scriptPath;
+
+if (packageJson.scripts[saturn]) {
     fs.writeFileSync(jsonPackagePath, JSON.stringify(packageJson, null, 2));
 } else {
     console.error("The property is non-existent.");
